248ba9a010d856dd987faf3c4ae965c2ccf97c1f
Gowthambaskaran510 <gowthambaskaran51095@gmail.com>
2024-02-17 18:18:38 +0000

    Solution

---
+++ src/pages/Chat/chat.js
@@ -0,0 +1,140 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-shadow */
+import {useEffect, useState} from 'react'
+import io from 'socket.io-client'
+import './chat.css'
+
+let socket
+const Chat = () => {
+  const [user, setUser] = useState('')
+  const [room, setRoom] = useState('')
+  const [users, setUsers] = useState([])
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  const socketUrl = 'http://localhost:8000'
+
+  useEffect(() => {
+    const {search} = window.location
+    const params = new URLSearchParams(search)
+
+    const user = params.get('name')
+
+    const room = params.get('room')
+
+    setUser(user)
+    setRoom(room)
+
+    socket = io(socketUrl)
+
+    socket.emit('join', {user, room}, err => {
+      if (err) {
+        // alert(err)
+      }
+    })
+
+    return () => {
+      // User leaves room
+      socket.disconnect()
+
+      socket.off()
+    }
+  }, [socketUrl])
+
+  useEffect(() => {
+    socket.on('message', msg => {
+      setMessages(prevMsg => [...prevMsg, msg])
+
+      setTimeout(() => {
+        const div = document.getElementById('chat_body')
+        div.scrollTop = div.scrollHeight - div.clientWidth
+      }, 10)
+    })
+
+    // eslint-disable-next-line no-shadow
+    socket.on('roomMembers', users => {
+      setUsers(users)
+    })
+  }, [])
+
+  const sendMessage = e => {
+    e.preventDefault()
+
+    socket.emit('sendMessage', message, () => setMessage(''))
+    setTimeout(() => {
+      const div = document.getElementById('chat_body')
+      div.scrollTop = div.scrollHeight
+    }, 100)
+  }
+
+  return (
+    <div className="container mt-4 ">
+      <div className="row chat-window" id="chat_window_1">
+        <div className="col-xs-4 col-md-4">
+          <p>Active Users</p>
+          <ul>
+            {users.map((e, i) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <li key={i}>{e.user}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-xs-8 col-md-8">
+          <div className="panel panel-default">
+            <div className="panel-heading top-bar">
+              <div className="col-md-12 col-xs-8">
+                <h3 className="panel-title">
+                  <span className="glyphicon glyphicon-comment" />
+                  {room}
+                </h3>
+              </div>
+            </div>
+            <div className="panel-body msg_container_base" id="chat_body">
+              {messages.map((e, i) =>
+                e.user === user?.toLowerCase() ? (
+                  <>
+                    <div key={i} className="row msg_container base_receive">
+                      <div className="col-xs-10 col-md-10">
+                        <div className="messages msg_receive">
+                          <p>{e.text}</p>
+                          <time>{e.user}</time>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div key={i} className="row msg_container base_sent">
+                      <div className="col-xs-10 col-md-10">
+                        <div className="messages msg_sent">
+                          <p>{e.text}</p>
+                          <time>{e.user}</time>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ),
+              )}
+            </div>
+            <div className="panel-footer">
+              <div className="input-group">
+                <input
+                  id="btn-input"
+                  type="text"
+                  value={message}
+                  onKeyPress={event =>
+                    event.key === 'Enter' ? sendMessage(event) : null
+                  }
+                  onChange={event => setMessage(event.target.value)}
+                  className="form-control input-sm chat_input"
+                  placeholder="Write your message here..."
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
